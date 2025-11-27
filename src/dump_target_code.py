a84e2c97d57dcb589c927dce88c2a06e8be1484b
revdoor <revdoor@kaist.ac.kr>
2025-11-28 00:51:31 +0900

    edit - simple_print in dump_target_code

--- src/dump_target_code.py
+++ src/dump_target_code.py
@@ -6,9 +6,15 @@ def simple_multiply(a, b):
     return a * b
 
 
+def simple_print(value):
+    print(value)
+
+
 def complex_formula(a, b, c, d):
     v1 = simple_add(a, b) - simple_multiply(c, d)
     v2 = simple_multiply(a, d) + simple_add(b, c)
+
+    simple_print(v1)
 
     result = simple_add(v1, v2)
 
