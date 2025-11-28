a04dbeceee077aab709ad5056cbef79a4b4ced2d
revdoor <revdoor@kaist.ac.kr>
2025-11-28 18:00:51 +0900

    remove - __init__ for CandidateGenerator
    - it would be just used to handle complex functions
    - so, there is no need of __init__

--- src/candidate_generator.py
+++ src/candidate_generator.py
@@ -76,9 +76,6 @@ class OrderVisitor(ast.NodeVisitor):
 
 
 class CandidateGenerator:
-    def __init__(self):
-        pass
-
     def generate_candidates(self, source_code):
         # TODO: Implement candidate generation logic
         # It should find various valid refactoring operators applicable to the source code
