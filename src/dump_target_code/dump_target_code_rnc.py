7b268a52940a2967b3f930f6a76ec46735455eea
revdoor <revdoor@kaist.ac.kr>
2025-11-30 08:25:57 +0900

    edit - more complicated dump code for RNC

--- src/dump_target_code/dump_target_code_rnc.py
+++ src/dump_target_code/dump_target_code_rnc.py
@@ -15,3 +15,12 @@ def also_my_func(a, b, c, d):
                     if i + j == 0:
                         if k + l == 0:
                             print("i+j and k+l are zero")
+
+
+def my_3rd_func(a, b, c):
+    if a == 0:
+        if b == 0:
+            if c == 0:
+                print("They are all zero!")
+    else:
+        print("a is not zero")
