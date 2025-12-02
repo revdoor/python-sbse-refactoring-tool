05bbe1173534f96d8d9def5ca6fd9afc81a725fc
revdoor <revdoor@kaist.ac.kr>
2025-12-02 14:32:01 +0900

    feat - testing more, about the effect of random name
    - result files added
    - t test result: no significant difference :(

--- src/util_llm.py
+++ src/util_llm.py
@@ -1,11 +1,10 @@
-from cmath import isnan
-
 import ollama
 import time
 import ast
+import re
 
 
-MODEL = 'llama3.2:3b'
+MODEL = 'llama3'
 
 
 def get_recommendations_for_rename(context_code, target_lines):
@@ -34,7 +33,6 @@ Names only, no 'def' or '='
 
 
 def get_recommendations_for_function_rename(function_code):
-    print(function_code)
     prompt = f"""Have a break, and then suggest better names for the given function. (Currently the name is random string)
 
 Code:
@@ -62,7 +60,8 @@ def readability_score(code):
 Your task is to emit a score from 0 to 100 based on the readability level of the source code presented.
 The response should start with 'Score: xx/100' format, and there would be some detailed explanations about that score. """
 
-    chat = f"""My code is here:
+    chat = f"""Mainly consider whether the name of the function is proper or not.
+My code is here:
 {code}
 """
 
@@ -74,92 +73,128 @@ The response should start with 'Score: xx/100' format, and there would be some d
         ]
     )
 
-    print(response['message']['content'].split('\n')[0])
+    return response['message']['content']
+
+
+def get_score(score_str):
+    try:
+        raw_val = re.search(r'Score: (\d+)/100', score_str).group(1)
+        return int(raw_val)
+    except:
+        return 0
 
 
 if __name__ == "__main__":
-#     start_time = time.time()
-#
-#     context = """def hehehe(a, b, c):
-#     if a == 0:
-#         return True
-#     if b == 0:
-#         return True
-#     if c == 0:
-#         return True
-#     return False
-#
-# def complex_formula(a, b, c):
-#     temp = a + b + c
-#
-#     if temp % 2 == 0:
-#         return True
-#     if temp % 3 == 0:
-#         return True
-#     return False
-#
-# def monte_carlo_pi(num_samples):
-#     v = 0
-#
-#     for _ in range(num_samples):
-#         x = random.uniform(-1, 1)
-#         y = random.uniform(-1, 1)
-#         if in_circle(x, y):
-#             v += 1
-#
-#     return (v / num_samples) * 4
-#     """
-#
-#     target_line_no = [0, 10, 19]
-#
-#     lines = context.split('\n')
-#     target_lines = [lines[no].strip() for no in target_line_no]
-#
-#     print("target lines are")
-#     for line in target_lines:
-#         print(line)
-#
-#     recommendations = get_recommendations_for_rename(context, target_lines)
-#
-#     print("Recommended names for the target line:")
-#     print(recommendations)
-#
-#     print(f"Execution Time: {time.time() - start_time:.2f} seconds")
-
-    ft_code = """def IlwRKbZAMcQnHzuWQiIy(a, b, c):
+    ft_codes = [
+        """def IlwRKbZAMcQnHzuWQiIy(a, b, c):
     if a == 0:
         return True
     if b == 0:
         return True
     if c == 0:
         return True
-    return False"""
+    return False""",
+        """def check_val_does_not_0(a, b, c):
+    if a == 0:
+        return True
+    if b == 0:
+        return True
+    if c == 0:
+        return True
+    return False
+    """,
+        """def check(num_str, key):
+    cnt = 0
+    length = len(key)
 
-    start_time = time.time()
+    for i in range(length):
+        if key[i] == num_str[i]:
+            cnt += 1
 
-    recommendations = get_recommendations_for_function_rename(ft_code)
+    return cnt""",
+        """def KvLsfVjrJXoRzwkdwyYG(num_str, key):
+    cnt = 0
+    length = len(key)
 
-    print("Recommended names:")
-    print(recommendations)
+    for i in range(length):
+        if key[i] == num_str[i]:
+            cnt += 1
 
-    print(f"Execution Time: {time.time() - start_time:.2f} seconds")
+    return cnt
+        """,
+        """def phi_guess(num_samples):
+    inside_circle = 0
 
-    root = ast.parse(ft_code)
+    for _ in range(num_samples):
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        if in_circle(x, y):
+            inside_circle += 1
 
-    ft_node = None
-    for node in ast.walk(root):
-        if isinstance(node, ast.FunctionDef):
-            ft_node = node
-            break
+    return (inside_circle / num_samples) * 4""",
+        """def BJvoXXfgvszIIOfFaIAn(num_samples):
+    inside_circle = 0
 
-    for _name in recommendations.split('\n')[0].split(','):
-        name = _name.strip()
+    for _ in range(num_samples):
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        if in_circle(x, y):
+            inside_circle += 1
 
-        ft_node.name = name
-        edited_code = ast.unparse(root)
+    return (inside_circle / num_samples) * 4"""
+    ]
 
-        start_time = time.time()
-        print(f"Check readability score for {name}...")
-        readability_score(edited_code)
-        print(f"Execution Time: {time.time() - start_time:.2f} seconds")
-        print()
+    for i in range(len(ft_codes)):
+        ft_code = ft_codes[i]
+
+        with open(f"score_result_{i}.txt", 'w') as f:
+            results = set()
+
+            for _ in range(10):
+                start_time = time.time()
+
+                recommendations = get_recommendations_for_function_rename(ft_code)
+
+                print("Recommended names:")
+                print(recommendations)
+
+                print(f"Execution Time: {time.time() - start_time:.2f} seconds")
+
+                for _name in recommendations.split('\n')[0].split(','):
+                    name = _name.strip()
+                    results.add(name)
+
+            print("The collected names are:")
+            print(results)
+
+            root = ast.parse(ft_code)
+
+            ft_node = None
+            for node in ast.walk(root):
+                if isinstance(node, ast.FunctionDef):
+                    ft_node = node
+                    break
+
+            std_scores = [get_score(readability_score(ft_code).split('\n')[0]) for _ in range(3)]
+            recommendation_scores = []
+
+            for name in results:
+                if not name:
+                    continue
+
+                ft_node.name = name
+                edited_code = ast.unparse(root)
+
+                print(f"Check readability score for {name}...")
+                for _ in range(3):
+                    start_time = time.time()
+                    res = readability_score(edited_code)
+                    print(res)
+                    print(f"Execution Time: {time.time() - start_time:.2f} seconds")
+                    print()
+
+                    score = get_score(res.split('\n')[0])
+                    recommendation_scores.append(score)
+
+            f.write(f"Score for origin: {std_scores}\n")
+            f.write(f"Score for recommendations: {recommendation_scores}\n\n")
