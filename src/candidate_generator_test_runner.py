2992957458209d23dd48ebfe2fb158800b726209
chojaehyun-x <cjhcjh0603@kaist.ac.kr>
2025-12-03 01:30:16 +0900

    fix - path issue

--- src/candidate_generator_test_runner.py
+++ src/candidate_generator_test_runner.py
@@ -1,11 +1,13 @@
 from candidate_generator import CandidateGenerator
 from type_enums import RefactoringOperatorType
+from pathlib import Path
 
 
 if __name__ == "__main__":
     for operator in RefactoringOperatorType:
         print(f"Run for {operator.value}...")
-        file = f'dump_target_code/dump_target_code_{operator.name.lower()}.py'
+        script_dir = Path(__file__).parent.resolve()
+        file = script_dir / f'dump_target_code/dump_target_code_{operator.name.lower()}.py'
 
         with open(file, 'r') as f:
             source_code = f.read()
