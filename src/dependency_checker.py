3da5c390b3e047d66cc50f925500c805e740df45
revdoor <revdoor@kaist.ac.kr>
2025-12-03 10:41:31 +0900

    feat - DependencyChecker skeleton

---
+++ src/dependency_checker.py
@@ -0,0 +1,10 @@
+"""
+This module defines the DependencyChecker class,
+which is used to check the dependency for the nodes between the surroundings.
+"""
+
+
+class DependencyChecker:
+    @staticmethod
+    def check_dependency(top_lvl_node, start_node, length):
+        pass
