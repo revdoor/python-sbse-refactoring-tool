b5d3a02e3787ec09d6252dad265283ecab67dcd9
revdoor <revdoor@kaist.ac.kr>
2025-12-04 14:41:18 +0900

    wip - dependency_free

--- src/dependency_checker.py
+++ src/dependency_checker.py
@@ -7,13 +7,63 @@ from pathlib import Path
 from type_enums import RefactoringOperatorType
 
 
+class DependencyVisitor(ast.NodeVisitor):
+    def __init__(self, ignore_nodes=None):
+        self.store_ids = set()
+        self.load_ids = set()
+        self.ignore_nodes = None
+
+    def visit_Name(self, node):
+        if self.ignore_nodes and node in self.ignore_nodes:
+            return
+
+        if isinstance(node.ctx, ast.Load):
+            self.load_ids.add(node.id)
+        else:
+            self.store_ids.add(node.id)
+
+    def generic_visit(self, node):
+        if self.ignore_nodes and node in self.ignore_nodes:
+            return
+
+        super().generic_visit(node)
+
+
 class DependencyChecker:
     @staticmethod
-    def check_dependency(top_lvl_node, start_node, length):
-        pass
+    def is_dependency_free(top_lvl_node, parent_node, attr_name, idx, length):
+        if not hasattr(attr_name, parent_node):
+            return False
+
+        attr = getattr(parent_node, attr_name)
+        if len(attr) < idx + length:
+            return False
+
+        target_nodes = attr[idx: idx + length]
+
+        target_store_ids = set()
+        target_load_ids = set()
+
+        for target_node in target_nodes:
+            for node in ast.walk(target_node):
+                store_ids, load_ids = DependencyChecker._dependency(node)
+
+                target_store_ids.update(store_ids)
+                target_load_ids.update(load_ids)
+
+        dependency_visitor = DependencyVisitor(target_nodes)
+        dependency_visitor.visit(top_lvl_node)
+
+        outer_store_ids = dependency_visitor.store_ids
+        outer_load_ids = dependency_visitor.load_ids
+
+        print("Target store IDs:", target_store_ids)
+        print("Target load IDs:", target_load_ids)
+        print("Outer store IDs:", outer_store_ids)
+        print("Outer load IDs:", outer_load_ids)
 
     @staticmethod
-    def dependency_name(node):
+    def _dependency(node):
         store_ids = set()
         load_ids = set()
 
@@ -26,8 +76,7 @@ class DependencyChecker:
                 else:
                     store_ids.add(_id)
 
-        print(store_ids)
-        print(load_ids)
+        return store_ids, load_ids
 
 
 if __name__ == "__main__":
@@ -44,4 +93,6 @@ if __name__ == "__main__":
         for _node in ast.walk(root):
             if isinstance(_node, ast.FunctionDef):
                 print(f"Function {_node.name}")
-                DependencyChecker.dependency_name(_node)
+                store_ids, load_ids = DependencyChecker._dependency(_node)
+                print("  Store IDs:", store_ids)
+                print("  Load IDs:", load_ids)
